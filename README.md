c7abd8785b773eb5e1d4781b281ea44abbdfdff2
Thibault Junin <spamfree@thibaultjunin.fr>
2017-08-06 17:33:39 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # i18n
 Internationalization library for PHP
+This library allows you to translate your website with ini files, Auto Detecting the language and much more!
+
+## Documentation
+
+### Information
+
+i18n will create a folder named "i18n" please make sur it is readable 
+
+### Installation
+
+1. Via composer: `composer require stantabcorp/i18n`
+2. Via manually download and install
+
+### Initialization
+
+In the i18n constructor your must have **3** parameters:
+1. The language you want to display your website **or** true to use AutoDetect
+2. The default language
+3. An array of available languages
+
+**Example:**
+Without AutoDetect: 
+```
+$i18n = new i18n("en", "en", ["en","fr"])
+```
+This will set the language to `en` (English), set the default language to `en` (English) and set that `en` (English) and `fr` (French) are available
+
+With AutoDetect: 
+```
+$i18n = new i18n(true, "en", ["en","fr"])
+```
+This will set the language to AutoDetect, set the default language and set available language for AutoDetect
+
+### Usage
+
+In order to get a translation you just need to:
+```
+$i18n->get("index in your file");
+```
+
+If you want to get the current language:
+```
+$i18n->getLang();
+```
+
+You want to change the language?
+```
+$i18n->setLang("language");
+```
+
+You don't want to use `sprintf`?! No problem:
+```
+$i18n->replace("string", [
+    "string" => "strong"
+]);
+```
+
+### File syntax
+
+**Sections are not yet supported by i18n**
+
+*Example file:*
+
+```
+word1 = "Some word"
+word2 = "Some other word"
+```
---
+++ src/i18n.php
@@ -0,0 +1,88 @@
+<?php
+
+    /**
+    * i18n
+    *
+    * Internationalization library for PHP 
+    * @author Thibault JUNIN <spamfree@thibaultjunin.fr>
+    * @copyright STAN-TAb Corp. 2017
+    * @link https://github.com/stantabcorp/i18n
+    * @license proprietary
+    */
+
+    class i18n{
+
+        private $lang;
+        private $default;
+        private $allow;
+
+        public function __construct($lang, $default, array $allow){
+            $this->default = $default;
+            $this->lang = ($lang === true) ? $this->autoDetect($allow) : $lang;
+            $this->allow = $allow;
+
+            if(!file_exists("./i18n/")){
+                mkdir("./i18n/");
+            }
+
+            if(!file_exists("./i18n/".$this->lang.".ini")){
+                file_put_contents("./i18n/".$this->lang.".ini", "; i18n file \n");
+            }
+
+            if(!file_exists("./i18n/".$this->default.".ini")){
+                throw new Exception("Default language not found !");
+            }
+        }
+
+        private function getLangArray(){
+            return parse_ini_file("./i18n/".$this->lang.".ini");
+        }
+
+        private function getDefaultArray(){
+            return parse_ini_file("./i18n/".$this->default.".ini");
+        }
+
+        public function get($word){
+            $lang = $this->getLangArray();
+            $default = $this->getDefaultArray();
+            if(isset($lang[$word])){
+                return $lang[$word];
+            }elseif(isset($default[$word])){
+                return $default[$word];
+            }else{
+                throw new Exception("Word not found !");
+            }
+        }
+
+        private function autoDetect($allowed){
+            if($allowed == null){
+                return $this->default;
+            }
+            $language = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+		    $language = $language{0}.$language{1};
+            if(in_array($language, $allowed)){
+                return $language;
+            }
+            return $this->default;
+        }
+
+        public function getLang(){
+            return $this->lang;
+        }
+
+        public function replace($str, array $data){
+            foreach($data as $key => $value){
+                $str = str_replace($key, $value, $str);
+            }
+            return $str;
+        }
+
+        public function setLang($lang){
+            if(in_array($lang, $this->allow)){
+                $this->lang = $lang;
+            }else{
+                $this->lang = $this->default;
+            }
+        }
+
+    }
